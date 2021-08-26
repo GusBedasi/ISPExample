d2b28ef97ad11d49cfd9642d20743053a1c46a57
GusBedasi <ggustavobedasi@gmail.com>
2021-08-26 20:19:47 -0300

    first commit

---
+++ ISPConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISPConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
